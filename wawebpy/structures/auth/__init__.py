c91f0092c69f77c520b595d82b473d497b8f0c94
cvcvka5 <cvcvka5@gmail.com>
2025-09-14 17:24:54 +0200

    removed LegacySessionAuth since its outdated

--- wawebpy/structures/auth/__init__.py
+++ wawebpy/structures/auth/__init__.py
@@ -1,7 +1,5 @@
 from ._noauth import NoAuth
-from ._baseauth import BaseAuth
 from ._localauth import LocalAuth
-from ._legacysessionauth import LegacySessionAuth 
 
 
-__all__ = [ "NoAuth", "LocalAuth", "LegacySessionAuth" ]
+__all__ = [ "NoAuth", "LocalAuth" ]
