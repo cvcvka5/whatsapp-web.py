e4a98f87d42525abde1b29ca6722acccd4ac8a83
cvcvka5 <cvcvka5@gmail.com>
2025-09-17 14:50:30 +0200

    new group class thats inherits the chat class

--- wawebpy/structures/group.py
+++ wawebpy/structures/group.py
@@ -1,7 +1,9 @@
 from __future__ import annotations
-from typing import TypedDict, List, Dict, Any
+from typing import TypedDict, List, Optional, Any, Dict
 from playwright.sync_api import Page
 from ..util import get_contact_script, get_module_script
+from .contact import Contact
+from .chat import Chat, ChatKwargs
 
 
 class GroupIDType(TypedDict, total=True):
@@ -10,45 +12,71 @@ class GroupIDType(TypedDict, total=True):
     _serialized: str
 
 
+class GroupParticipantObject(TypedDict):
+    id: GroupIDType
+    phoneNumber: GroupIDType
+    username: str
+    isAdmin: bool
+    isSuperAdmin: bool
+    
+
 class GroupMetadata(TypedDict, total=False):
-    id: str
-    owner: str
+    id: GroupIDType
+    owner: GroupIDType
     subject: str
     creation: int
-    participants: List[Dict[str, Any]]  # jid + isAdmin/role flags etc.
+    participants: List[GroupParticipantObject]
+    subjectOwner: GroupIDType
+    subjectTime: int
+    size: int
 
+    # Feature toggles / restrictions
+    allowNonAdminSubGroupCreation: bool
+    announce: bool
+    restrict: bool
+    incognito: bool
+    generalChatAutoAddDisabled: bool
+    generalSubgroup: bool
+    defaultSubgroup: bool
+    membershipApprovalMode: bool
+    reportToAdminMode: bool
+    noFrequentlyForwarded: bool
+    suspended: bool
+    support: bool
 
-class GroupKwargs(TypedDict, total=True):
-    id: GroupIDType
-    name: str
-    formattedTitle: str
-    groupMetadata: GroupMetadata
-    isAnnounceGrpRestrict: bool
-    unreadCount: int
-    t: int
-    mute: Dict[str, Any]
-    muteExpiration: int
-    archive: bool
-    disappearingModeTrigger: str
-    ephemeralDuration: int
-    notSpam: bool
-    trusted: bool
+    # Extra details
+    creatorCountryCode: Optional[str]
+    creatorUsername: Optional[str]
+    subjectOwnerUsername: Optional[str]
+    numSubgroups: Optional[int]
+    membershipApprovalRequest: Optional[Any]
+    hasCapi: bool
+    hasIncompleteParticipantInformation: bool
+    isLidAddressingMode: bool
+    isParentGroup: bool
+    isParentGroupClosed: bool
+    memberAddMode: str
+    trusted: Optional[bool]
 
+class GroupKwargs(ChatKwargs, total=True):
+    mentionName: str
+    
+    isMe: bool
+    isBot: bool
+    isContact: bool
 
-class Group:
-    __attribute_map = {
-        "getId": "id",  
-        "getName": "name",
+class Group(Chat):
+    _attribute_map = {
+        # Attributes
         "getMentionName": "mentionName",
         
         # Flags
         "getIsMe": "isMe",
-        "getIsGroup": "isGroup",
         "getIsBot": "isBot",
         "getIsWAContact": "isContact"
     }
 
-    def __init__(self, page: Page, **kwargs: GroupKwargs):
+    def __init__(self, page: Page, **kwargs: Group):
         self._page: Page = page
 
         # Identifiers
@@ -58,8 +86,8 @@ class Group:
         
         
         # Flags
-        self._is_me: bool = kwargs["isMe"]
         self._is_group: bool = kwargs["isGroup"]
+        self._is_me: bool = kwargs["isMe"]
         self._is_bot: bool = kwargs["isBot"]
         self._is_contact: bool = kwargs["isContact"]
         
@@ -67,15 +95,28 @@ class Group:
     @staticmethod
     def get(page: Page, jid: str) -> Group:
         attrs = {}
-        for func_name, attr_name in Group.__attribute_map.items():
-            group_script = get_contact_script(jid)
+        # Get general chat attributes
+        group_script = get_contact_script(jid)
+        for func_name, attr_name in Chat._attribute_map.items():
             script = get_module_script(
-                module="WaWebContact",
+                module="WAWebChatGetters",
                 function=func_name,
                 args=(group_script,),
             )
             attr_value = page.evaluate(script)
             attrs[attr_name] = attr_value
+
+        # Get group specific attributes
+        for func_name, attr_name in Group._attribute_map.items():
+            script = get_module_script(
+                module="WAWebContactGetters",
+                function=func_name,
+                args=(group_script,),
+            )
+            attr_value = page.evaluate(script)
+            attrs[attr_name] = attr_value
+
+        # Get metadata
 
         return Group(page, **attrs)
 
@@ -85,32 +126,39 @@ class Group:
         self.__dict__.update(new_group.__dict__)
 
     # --- Properties ---
-    @property
-    def page(self) -> Page:
-        return self._page
+    def get_metadata(self) -> GroupMetadata:
+        script = get_module_script(
+            module="WAWebGroupQueryJob",
+            function="queryGroupsById",
+            args=("["+self._js_variable_repr('id')+"['_serialized']]",)
+        )
+        metadata = self.page.evaluate(script)[0]
+        
+        return metadata
 
-    @property
-    def id(self) -> GroupIDType:
-        return self._id
-
-    @property
-    def jid(self) -> str:
-        return self._id["_serialized"]
-
-    @property
-    def name(self) -> str:
-        return self._name
-
+    def get_participants(self) -> List[Contact]:
+        metadata = self.get_metadata()
+        participant_objects = metadata.get("participants", [])
+        
+        participants = []
+        for obj in participant_objects:
+            _id = obj.get("id", None)
+            if not _id: continue
+            _jid = _id.get("_serialized", None)
+            if not _jid: continue
+            
+            participants.append(Contact.get(self.page, _jid))
+            
+        return participants
+        
+    
     @property
     def mention_name(self) -> str:
         return self._mention_name
     
+    @property
     def is_me(self) -> bool:
         return self._is_me
-
-    @property
-    def is_group(self) -> bool:
-        return self._is_group
 
     @property
     def is_bot(self) -> bool:
@@ -121,4 +169,7 @@ class Group:
         return self._is_contact
 
     def __str__(self):
-        return f"Group({self.formatted_title}, {self.jid})"
+        return f"Group({self.name}, {self.jid})"
+
+    def __repr__(self):
+        return f"Group({self.name}, {self.jid})"
